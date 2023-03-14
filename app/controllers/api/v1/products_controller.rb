5003a8a6abdb0e83f156a44b9c6698a86cb7e871
benessilfie <bquansah007@gmail.com>
2023-03-14 00:19:18 +0000

    Added product endpoint and corresponding actions

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,50 @@
+class API::V1::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show update destroy]
+  before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update destroy]
+
+  def index
+    render json: Product.all
+  end
+
+  def show
+    render json: @product
+  end
+
+  def create
+    product = current_user.products.build(product_params)
+
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: { errors: @product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def check_owner
+    head :forbidden unless @product.user_id == current_user&.id
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
+  end
+end
