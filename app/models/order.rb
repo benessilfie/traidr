327db061b605bd613faa1d8d165b7b233abe7c56
benessilfie <bquansah007@gmail.com>
2023-03-15 12:53:51 +0000

    set total for product orders before validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,10 @@ class Order < ApplicationRecord
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
-  validates :total, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  before_validation :set_total!
+  validates :total, presence: true
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
