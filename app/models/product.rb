91b33175670ffa281f015f8595daf0931da554da
benessilfie <bquansah007@gmail.com>
2023-03-15 10:03:13 +0000

    Implemented the various search scopes for the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,20 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 
   belongs_to :user
+
+  scope :filter_by_title, ->(keyword) { where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
+  scope :above_or_equal_to_price, ->(price) { where('price >= ?', price) }
+  scope :below_or_equal_to_price, ->(price) { where('price <= ?', price) }
+  scope :recent, -> { order(:updated_at) }
+
+  def self.search(params = {})
+    products = params[:product_ids].present? ? Product.where(id: params[:product_ids]) : Product.all
+
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price].to_f) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price].to_f) if params[:max_price]
+    products = products.recent if params[:recent].present?
+
+    products
+  end
 end
