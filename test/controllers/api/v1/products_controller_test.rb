dfeffac583be7698e301d4d31451778766ccd06c
benessilfie <bquansah007@gmail.com>
2023-03-16 00:25:27 +0000

    feat: Implementated pagination for products to optimise response

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -7,6 +7,12 @@ class API::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'should show products' do
     get api_v1_products_url, as: :json
     assert_response :success
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert_not_nil json_response.dig(:links, :first)
+    assert_not_nil json_response.dig(:links, :last)
+    assert_not_nil json_response.dig(:links, :prev)
+    assert_not_nil json_response.dig(:links, :next)
   end
 
   test 'should show product' do
