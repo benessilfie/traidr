ea350d272967212ba41f0522c8eb19998bcf6a63
benessilfie <bquansah007@gmail.com>
2023-03-16 00:09:39 +0000

    Decreases the product quantity by the placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,10 @@
 class Placement < ApplicationRecord
   belongs_to :order
   belongs_to :product, inverse_of: :placements
+
+  after_create :decrement_product_quantity!
+
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
