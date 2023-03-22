584c3eb793eddac94545cdec22a955bb52c7b68d
benessilfie <bquansah007@gmail.com>
2023-03-22 00:02:15 +0000

    remove subdomain constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api, path: '', constraints: { subdomain: 'api' }, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :products
